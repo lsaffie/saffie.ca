5486456263748c966a3e75a971cc2aafe92978a9
Luis Saffie <luis.saffie@ehealthinnovation.org>
2010-10-12 08:20:08 -0400

    including first part of url in email

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     comment = Comment.new(params[:comment])
     if @post.comments << comment
-      Pony.mail(:to => 'luis.saffie@gmail.com', :from => 'comments@saffie.ca', :subject => 'Comment', :body => "#{comment.body} #{post_comment_path(@post, comment)}")
+      Pony.mail(:to => 'luis.saffie@gmail.com', :from => 'comments@saffie.ca', :subject => 'Comment', :body => "#{comment.body} www.saffie.ca/#{post_comment_path(@post, comment)}")
       redirect_to @post
     end
   end
