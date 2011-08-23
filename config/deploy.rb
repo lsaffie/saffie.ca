f648c8b59e439f0b8bd97cbf36662730622af2c4
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-08-23 12:44:28 -0400

    Squashed commit of the following:
    commit fefb2239aa9290f19bd59f3485ab1771854b04ee
    Author: Luis Saffie <luis.saffie@ehealthinnovation.org>
    Date:   Tue Aug 23 12:44:10 2011 -0400
    
        capifycation

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+require 'bundler/capistrano'
+
+## This get cap to see my bundled gems
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+#################
+
+set :application, "saffie.ca"
+set :repository,  "git@github.com:lsaffie/saffie.ca.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "192.75.165.55"                          # Your HTTP server, Apache/etc
+role :app, "192.75.165.55"                          # This may be the same as your `Web` server
+role :db,  "192.75.165.55", :primary => true # This is where Rails migrations will run
+role :db,  "192.75.165.55"
+
+set :user, "deploy"
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_via, :remote_cache
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
