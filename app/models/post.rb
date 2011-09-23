5461675b1553fb1ad55d0309b27d3ad6568a28fe
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-09-23 09:04:41 -0400

    defining to_params for url friendly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments, :order => "created_at DESC"
   has_and_belongs_to_many :tags
+
+  def to_param
+    "#{id}-#{title.gsub(/[^a-z0-9]+/i, '-')}"
+  end
 end
