a4a0869082606f3c437112617c6116947f04d454
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-10-16 19:26:52 -0400

    touch fix

--- public/links/html5/touch-events/script.js
+++ public/links/html5/touch-events/script.js
@@ -10,10 +10,21 @@ function move(obj) {
   }, false);
 }
 
+function make_circles(canvas, color) {
+  var ctx = canvas.getContext('2d');
+  ctx.beginPath();
+  ctx.arc(100,100,50,0,2*Math.PI, false);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
 window.onload = function() {
   var obj1= document.getElementById('canvas-touch1');
+  make_circles(obj1, "red");
   var obj2= document.getElementById('canvas-touch2');
+  make_circles(obj2,"yellow");
   var obj3= document.getElementById('canvas-touch3');
+  make_circles(obj3, "blue");
   move(obj1);
   move(obj2);
   move(obj3);
